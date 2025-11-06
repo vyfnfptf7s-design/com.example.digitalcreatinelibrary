402b00607627b559690d5c9b95bf6fab2368ef77
talya <talyahabasov@gmail.com>
2025-11-06 18:49:58 +0200

    first commit

---
+++ app/src/main/java/com/example/digitalcreatinelibrary/Creator_Library_Select_a_Genre.java
@@ -0,0 +1,24 @@
+package com.example.digitalcreatinelibrary;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Creator_Library_Select_a_Genre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_creator_library_select_agenre);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/com/example/digitalcreatinelibrary/LoginActivity.java
@@ -0,0 +1,4 @@
+package com.example.digitalcreatinelibrary;
+
+public class LoginActivity {
+}
